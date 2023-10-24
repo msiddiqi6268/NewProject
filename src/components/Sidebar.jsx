7839d0233bd5831382ab21b97b2f7083aacd5d82
msiddiqi6268 <msiddiqi6268@gmail.com>
2023-10-25 00:16:05 +0500

    desktop tour added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Joyride from 'react-joyride'
+import 'intro.js/introjs.css';
 import '../styles/Sidebar.css'
 import { Home } from '@mui/icons-material'
 import { Box, Button } from '@mui/material'
@@ -7,41 +9,81 @@ import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
 const Sidebar = () => {
+    const [runTour, setRunTour] = useState(true);
+    const steps = [
+        {
+            target: '.sidebar-button:nth-child(1)',
+            disableBeacon: true,
+            content: <div className = 'tour-box'><h3>Take a quick tour around?</h3><p>Use dashboard panel to access more options.</p></div>,  
+        },
+        {
+            target: '.sidebar-button:nth-child(2)',
+            content: <div className = 'tour-box'><h3>Take a quick tour around?</h3><p>Use dashboard panel to access more options.</p></div>,  
+        },
+        {
+            target: '.sidebar-button:nth-child(3)',
+            content: <div className = 'tour-box'><h3>Take a quick tour around?</h3><p>Use dashboard panel to access more options.</p></div>,  
+        },
+        {
+            target: '.sidebar-button:nth-child(4)',
+            content: <div className = 'tour-box'><h3>Take a quick tour around?</h3><p>Use dashboard panel to access more options.</p></div>,  
+        },
+        
+    ];
     const items = [
         {
-            icon:<Home  className = 'sidebar-button-icon' />,
-            text:"Home"
+            icon: <Home className='sidebar-button-icon' />,
+            text: "Home"
         },
         {
-            icon:<WifiTetheringIcon  className = 'sidebar-button-icon' />,
-            text:"Live Session"
+            icon: <WifiTetheringIcon className='sidebar-button-icon' />,
+            text: "Live Session"
         },
         {
-            icon:<FileUploadOutlinedIcon  className = 'sidebar-button-icon' />,
-            text:"Upload  Video"
+            icon: <FileUploadOutlinedIcon className='sidebar-button-icon' />,
+            text: "Upload  Video"
         },
         {
-            icon:<WifiTetheringIcon  className = 'sidebar-button-icon' />,
-            text:"Analyze Data"
+            icon: <WifiTetheringIcon className='sidebar-button-icon' />,
+            text: "Analyze Data"
         },
     ]
-  return (
-    <Box className='sidebar-container'>
-        <Box className ='sidebar-logo'></Box>
-        <Box>
-            {items.map((item,index)=>(
-                <Button variant = 'text' className = 'sidebar-button' key = {index}>
-                    {item.icon}
-                    {item.text}
-                </Button>
-            ))}
-        </Box>
-        <Button variant = 'text' className = 'sidebar-button'>
-                    {<LogoutRoundedIcon className = 'sidebar-button-icon' />}
-                    {"Logout"}
+    return (
+        <Box className='sidebar-container'>
+
+            <Box className='sidebar-logo'></Box>
+            <Box>
+                {items.map((item, index) => (
+                    <Button variant='text' className={`sidebar-button tour-${index}`} key={index}>
+                        {item.icon}
+                        {item.text}
+                    </Button>
+                ))}
+            </Box>
+            <Button variant='text' className='sidebar-button'>
+                {<LogoutRoundedIcon className='sidebar-button-icon' />}
+                {"Logout"}
             </Button>
-    </Box>
-  )
+            <Joyride
+                // enabled={true}
+                steps={steps}
+                run = {runTour}
+                continuous
+                disableBeaco = {true}
+                styles={{
+                    options: {
+                    // color:'white',
+                      arrowColor: '#0d8de9',
+                      backgroundColor: '#0d8de9',
+                      overlayColor: 'rgba(79, 26, 0, 0.4)',
+                      primaryColor: 'var(--light-background)',
+                      textColor: 'black',
+                    }
+                  }}
+                // initialStep={0}
+            />
+        </Box>
+    )
 }
 
 export default Sidebar
--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Joyride from 'react-joyride';
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import { Box, Typography } from '@mui/material'
 import '../styles/Dashboard.css'
 import { LineChart } from '@mui/x-charts/LineChart';
 import { BarChart } from '@mui/x-charts/BarChart';
-function linspace(start, stop, num, endpoint = true) {
-    const div = endpoint ? (num - 1) : num;
-    const step = (stop - start) / div;
-    return Array.from({ length: num }, (_, i) => start + step * i);
-}
 
-const uData = [1, 7, 3, 10, 6, 1, 4, 8, 2, 9, 0, 4, 4, 0, 9, 9, 6, 7, 6, 2, 6, 3, 3, 0, 9, 5, 5, 7, 6, 2, 8, 0, 6, 8, 0, 7, 0, 5, 9, 10, 7, 3, 1, 7, 7, 2, 2, 1, 9, 3, 2, 3, 7, 10, 2, 4, 0, 2, 1, 9, 1, 2, 4, 5, 1, 2, 10, 9, 6, 3, 7, 3, 9, 6, 6, 2, 4, 2, 5, 3, 6, 6, 8, 2, 5, 10, 5, 5, 6, 8, 8, 7, 6, 5, 5, 2, 2, 8, 9, 2, 10, 10, 8, 4, 6, 2, 10, 4, 0, 7, 4, 10, 6, 8, 8, 0, 1, 5, 3, 6, 6, 3, 9, 0, 9, 2, 2, 6, 2, 1, 5, 10, 9, 4, 7, 3, 10, 0, 10, 9, 2, 10, 10, 0, 9, 7, 8, 1, 5, 1, 9, 8, 0, 2, 7, 6, 9, 8, 7, 9, 8, 2, 9, 5, 9, 8, 10, 8, 4, 6, 7, 6, 1, 5, 10, 3, 3, 2, 9, 6, 2, 1, 2, 8, 4, 3, 10, 3, 5, 2, 4, 9, 2, 7, 3, 10, 4, 4, 5, 6]
-const pData = [4, 8, 2, 6, 10, 3, 1, 9, 0, 1, 1, 5, 10, 2, 3, 8, 6, 5, 10, 5, 4, 0, 4, 1, 1, 0, 5, 4, 7, 0, 5, 10, 4, 6, 2, 5, 7, 10, 0, 7, 8, 9, 9, 8, 7, 2, 0, 9, 10, 1, 4, 8, 4, 5, 2, 0, 6, 9, 8, 1, 9, 5, 5, 9, 7, 3, 3, 2, 5, 9, 6, 3, 10, 5, 4, 5, 4, 4, 4, 3, 10, 9, 9, 3, 6, 2, 9, 0, 7, 4, 0, 7, 7, 4, 10, 9, 6, 7, 1, 8, 1, 2, 0, 4, 8, 1, 2, 2, 3, 10, 6, 10, 5, 5, 9, 0, 8, 3, 0, 9, 8, 5, 6, 0, 4, 7, 6, 0, 1, 2, 0, 7, 5, 10, 9, 2, 1, 6, 5, 6, 7, 7, 5, 0, 7, 2, 0, 4, 8, 7, 10, 4, 6, 5, 2, 10, 3, 10, 6, 10, 2, 7, 7, 4, 3, 6, 7, 7, 1, 4, 0, 1, 2, 3, 0, 0, 10, 7, 6, 10, 2, 3, 0, 3, 5, 1, 7, 8, 10, 7, 6, 3, 5, 1, 9, 0, 4, 10, 10, 7]
+const uData = [36, 11, 58, 74, 77, 69, 38, 54, 97, 12, 73, 44, 34, 43, 94, 95, 33, 83]
+const pData = [12, 27, 87, 15, 63, 13, 41, 87, 71, 81, 92, 83, 93, 52, 53, 47, 88, 81]
+const aData = [32, 81, 91, 16, 68, 65, 95, 93, 98, 69, 28, 89, 86, 48, 14, 28, 56, 1]
+const dData = [99, 99, 65, 97, 74, 92, 45, 30, 81, 27, 47, 53, 76, 52, 45, 45, 13, 81]
 const Dashboard = () => {
+    
+      
+    
     return (
         <div>
             <Navbar />
             <Box className='dashboard' >
-                <Sidebar />
+                <Sidebar className = 'dashboard-sidebar' />
                 <Box className='DashboardContent'>
                     <Box className='line-chart'>
                 <Typography variant='h4'>Live Sessions</Typography>
@@ -27,6 +28,8 @@ const Dashboard = () => {
                             series={[
                                 { data: pData, label: 'pv' },
                                 { data: uData, label: 'uv' },
+                                { data: aData, label: 'ad' },
+                                { data: dData, label: 'dd' },
                             ]}
                         />
                     </Box>
