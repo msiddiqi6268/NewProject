516eb9aada2cc2ea87bcd8f92644da1e8dd44505
Muhammad Siddiqi <muhammad.siddiqi@curemd.com>
2023-10-25 20:42:25 +0500

    analyze data comp

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AuthPages from './containers/AuthPages'
 import Dashboard from './containers/Dashboard'
 import LiveSession from './containers/LiveSession'
 import UploadVideo from './containers/UploadVideo'
+import AnalyzeData from './containers/AnalyzeData'
 const App = () => {
   return (
     <BrowserRouter>
@@ -17,6 +18,7 @@ const App = () => {
       <Route exact path = '/dashboard' element = {<Dashboard />} />
       <Route exact path = '/live-session' element = {<LiveSession />} />
       <Route exact path = '/upload-video' element = {<UploadVideo />} />
+      <Route exact path = '/analyze-data' element = {<AnalyzeData />} />
     </Routes>
     </BrowserRouter>
   )
